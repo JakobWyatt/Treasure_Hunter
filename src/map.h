1a3ba69fc103c1ac031a0c55088a8dddc42326d9
Jakob Wyatt <jakob.wyatt@gmail.com>
2019-04-26 23:25:47 +0800

    added function declarations for split and read_line

--- src/map.h
+++ src/map.h
@@ -52,4 +52,24 @@ USAGE: Imports rows, cols, and file. Exports read_into and status.
 */
 status fill_map(map read_into, size_t rows, size_t cols, FILE* file);
 
+/*
+PURPOSE: Reads a line from a file.
+USAGE: Reads from a file until newline or EOF is encountered.
+    Newline is not included in the returned string.
+    If an attempt to read from the file immediately results in EOF,
+        an empty string is returned.
+    The returned string must be freed after use.
+*/
+char* read_line(FILE* file);
+
+/*
+PURPOSE: Splits a string with the given delimiter.
+USAGE: tokens is a pointer to an array of char*.
+    Each token is stored in tokens as it is parsed.
+    If the number of tokens is not equal to tokens_sz, ABORTED is returned.
+    Otherwise, COMPLETE is returned.
+    Strings stored in tokens remain in scope as long as line remains in scope.
+*/
+status split(char* line, char delim, char** tokens, size_t tokens_sz);
+
 #endif
