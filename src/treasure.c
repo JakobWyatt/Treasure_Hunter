86010008364fd39b436916e0cc511bad79878ff7
Jakob Wyatt <jakob.wyatt@gmail.com>
2019-04-26 21:51:05 +0800

    implemented compare and swap functions for treasure

--- src/treasure.c
+++ src/treasure.c
@@ -1,3 +1,40 @@
 #include "treasure.h"
 
+void swap(treasure* a, treasure* b)
+{
+    treasure tmp = *a;
+    *a = *b;
+    *b = tmp;
+}
 
+void compareHead(treasure* gear, explorer* person)
+{
+    if (gear->value > person->head.value)
+    {
+        swap(gear, &person->head);
+    }
+}
+
+void compareChest(treasure* gear, explorer* person)
+{
+    if (gear->value > person->chest.value)
+    {
+        swap(gear, &person->chest);
+    }
+}
+
+void compareLegs(treasure* gear, explorer* person)
+{
+    if (gear->value > person->legs.value)
+    {
+        swap(gear, &person->legs);
+    }
+}
+
+void compareHands(treasure* gear, explorer* person)
+{
+    if (gear->value > person->hands.value)
+    {
+        swap(gear, &person->hands);
+    }
+}
