f47f56ae2e72510ebe24263a63b1eb5f2a2e37f7
Jakob Wyatt <jakob.wyatt@gmail.com>
2019-04-26 23:14:43 +0800

    added implementation of fill_map

--- src/map.c
+++ src/map.c
@@ -1,12 +1,12 @@
 #include "map.h"
 
 #include <stdio.h>
-#include <stdlib.h>
+#include <stdlib.h> /*malloc*/
 
 status read_map(map* read_into, size_t* rows, size_t* cols, char* filename)
 {
     status result = COMPLETE;
-    int result;
+    int args_read;
     FILE* file = fopen(filename, "r");
     if (file == NULL)
     {
@@ -16,8 +16,8 @@ status read_map(map* read_into, size_t* rows, size_t* cols, char* filename)
     } else
     {
         /*Note: fscanf does not protect against integer overflow.*/
-        result = fscanf(file, "%zu,%zu", rows, cols);
-        if (result != 2)
+        args_read = fscanf(file, "%zu,%zu", rows, cols);
+        if (args_read != 2)
         {
             fprintf(stderr, "Incorrect formatting in %s, line 1: Expected <rows>,<cols>\n", filename);
             result = ABORTED;
@@ -77,6 +77,56 @@ map allocate_map(size_t rows, size_t cols)
 
 status fill_map(map read_into, size_t rows, size_t cols, FILE* file)
 {
+    size_t i = 0;
+    size_t j = 0;
+    char* line;
+    char** tokens = (char**)malloc(sizeof(char*) * cols);
     status result = COMPLETE;
-    
+    /*As soon as incorrect formatting is encountered, stop reading the file*/
+    while (i < rows && result == COMPLETE)
+    {
+        /*To fill the map, we will:
+            -Read the line.
+            -Split the line using split.
+            -Use make_treasure to construct a new treasure, and add it to the map.
+
+            strtok cannot be used, as it skips over repeated delimiters.
+            Creating a function to read up until a delimiter can be used,
+                however it complicates matters when the file does not end with a newline.
+        */
+        line = read_line(file);
+        result = split(tokens, cols, line, ',');
+        if (result == COMPLETE)
+        {
+            /*The token array has been filled.*/
+            while (j < cols && result == COMPLETE)
+            {
+                result = make_treasure(tokens[j], read_into[i] + j);
+                ++j;
+            }
+        } else
+        {
+            /*If we have reached EOF on the file, there is an incorrect number of rows.
+            Otherwise, there is an incorrect number of columns.*/
+            if (feof(file))
+            {
+                fprintf(stderr, "Incorrect number of rows: read %zu, expected %zu.\n", i, rows);
+            } else
+            {
+                fprintf(stderr, "Incorrect number of columns at row %zu: expected %zu.\n", i + 1, cols);
+            }
+        }
+        ++i;
+        j = 0;
+        free(line);
+    }
+    free(tokens);
+
+    return result;
 }
+/*
+char* read_line(file)
+{
+
+}
+*/
