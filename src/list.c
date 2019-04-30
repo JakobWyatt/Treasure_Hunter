0ea77e4c5b0aa85d666e9620ca6d99fe7abe1bc2
Jakob Wyatt <jakob.wyatt@gmail.com>
2019-04-30 15:06:23 +0800

    implemented node deletion

--- src/list.c
+++ src/list.c
@@ -1,3 +1,30 @@
 #include "list.h"
 
+#include <stddef.h> /*NULL*/
 
+void insert(list x, node* iter, void* data);
+
+void remove(list x, node* iter)
+{
+    free(iter->data);
+    /*If the node is head or tail, change list pointers*/
+    /*Otherwise, reroute neighbouring nodes*/
+    if (iter->prev == NULL)
+    {
+        x.head = iter->next;
+    } else {
+        iter->prev->next = iter->next;
+    }
+    if (iter->next == NULL)
+    {
+        x.tail = iter->prev;
+    } else {
+        iter->next->prev = iter->prev;
+    }
+
+    free(iter);
+}
+
+list make_list();
+
+void free_list(list x);
