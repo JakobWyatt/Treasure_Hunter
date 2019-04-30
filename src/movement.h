18762dba7654ea513beecccede8aa3bd5d14eedc
Jakob Wyatt <jakob.wyatt@gmail.com>
2019-04-30 22:52:59 +0800

    added declaration for parse_movement function

--- src/movement.h
+++ src/movement.h
@@ -12,4 +12,15 @@ and ABORTED is returned.
 */
 status read_moves(list* moves, char* filename);
 
+/*
+PURPOSE: Parse a movement line, and add it to the linked list.
+USAGE: Imports a string that optionally ends with a newline.
+Exports moves. line must not be NULL.
+If there is an error in the line, a message is printed to stderr,
+and ABORTED is returned.
+This function modifies moves on failure, and therefore does not have
+strong exception safety.
+*/
+status parse_movement(list* moves, char* line);
+
 #endif
