04713a937eb42b5de63ff6900f415903e8538e58
Jakob Wyatt <jakob.wyatt@gmail.com>
2019-04-30 22:53:22 +0800

    added definition for parse_movement function

--- src/movement.c
+++ src/movement.c
@@ -61,3 +61,45 @@ status read_moves(list* moves, char* filename)
 
     return result;
 }
+
+status parse_movement(list* moves, char* line)
+{
+    /*Does not check for broken allocation*/
+    status result = COMPLETE;
+    char* space = strchr(line, ' ');
+    int args_read;
+
+    /*We do not guarentee strong exception safety, so allocation and
+    addition to the list can be done immidiately. This improves the chance
+    we get memory safety right.*/
+    move* data = (move*)malloc(sizeof(move));
+    insert(*moves, NULL, data);
+
+    if (space == NULL)
+    {
+        fprintf(stderr, "Invalid formatting\n");
+        result = ABORTED;
+    } else
+    {
+        *space = '\0';
+        data->dir = choose_dir(line);
+        args_read = sscanf(space + 1, " %d ", &data->distance);
+    }
+
+    /*Check for errors*/
+    if (args_read != 1)
+    {
+        fprintf(stderr, "Distance must be an integer\n");
+        result = ABORTED;
+    } else if (data->distance < 0)
+    {
+        fprintf(stderr, "Distance must be positive\n");
+        result = ABORTED;
+    } else if (data->dir == INVALID)
+    {
+        fprintf(stderr, "Invalid direction\n");
+        result = ABORTED;
+    }
+
+    return result;
+}
