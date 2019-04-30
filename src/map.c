bdcdea534d8a8096a3c2df709a82e26a56e17874
Jakob Wyatt <jakob.wyatt@gmail.com>
2019-04-30 09:51:53 +0800

    added error messages for incorrect treasure formatting, fixed end of line bug

--- src/map.c
+++ src/map.c
@@ -2,6 +2,7 @@
 
 #include <stdio.h>
 #include <stdlib.h> /*malloc*/
+#include <string.h> /*strchr*/
 
 status read_map(map* read_into, size_t* rows, size_t* cols, char* filename)
 {
@@ -102,6 +103,12 @@ status fill_map(map read_into, size_t rows, size_t cols, FILE* file)
             while (j < cols && result == COMPLETE)
             {
                 result = make_treasure(tokens[j], read_into[i] + j);
+                /*make_treasure will print an error if formatting is incorrect.
+                However, it will not print where this error occured.*/
+                if (result != COMPLETE)
+                {
+                    fprintf(stderr, "At row %zu, column %zu.\n", i + 1, j + 1);
+                }
                 ++j;
             }
         } else
@@ -138,6 +145,7 @@ char* read_line(FILE* file)
     str[size - 1] = 1;
     fgets(str, size, file);
     char* error = str;
+    char* end;
 
     while (str[size - 1] == '\0' && error != NULL)
     {
@@ -156,6 +164,13 @@ char* read_line(FILE* file)
     if (error == NULL)
     {
         str[0] = '\0';
+    }
+
+    /*Remove any newlines at the end of the string*/
+    end = strchr(str, '\n');
+    if (end != NULL)
+    {
+        *end = '\0';
     }
 
     return str;
