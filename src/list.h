26c277105defea90ca175d02dd447ad3ad62c5e9
Jakob Wyatt <jakob.wyatt@gmail.com>
2019-04-30 15:06:11 +0800

    added declarations for linked list interface

--- src/list.h
+++ src/list.h
@@ -4,10 +4,10 @@
 typedef struct node_s node;
 
 /*
-PURPOSE: Type describing a node in a linked list.
+PURPOSE: Type describing a node in a doubly linked list.
 IMPLEMENTATION: Contains a void pointer to the data stored in the node,
-    and a pointer to the next node. The pointer to the next node is NULL
-    if it is the last node in the list.
+    a pointer to the next node, and a pointer to the previous node.
+    The pointer to the next/previous node is NULL if it is the last/first node in the list.
 USAGE: The data stored in the node must be manually dynamically allocated,
     and the type of the data stored in the node is maintained by the user.
 */
@@ -15,18 +15,45 @@ struct node_s
 {
     void* data;
     node* next;
+    node* prev;
 };
 
 /*
 PURPOSE: Type describing a linked list.
-IMPLEMENTATION: Contains a pointer to the first element in the list.
-    In future, a pointer to the last element in the list may be added,
-    to speed up inserts.
+IMPLEMENTATION: Contains a pointer to the first element in the list
+    and a pointer to the last element in the list.
 USAGE: Use the functions declared in list.h to manipulate the list.
 */
 typedef struct
 {
     node* head;
+    node* tail;
 } list;
+
+/*
+PURPOSE: Insert an element before the given element.
+USAGE: data must be dynamically allocated.
+If iter == NULL, insert the element at the end of the list.
+*/
+void insert(list x, node* iter, void* data);
+
+/*
+PURPOSE: Deallocates and removes the given element.
+USAGE: Pass a pointer to the element to remove.
+*/
+void remove(list x, node* iter);
+
+/*
+PURPOSE: Creates an empty list.
+USAGE: Use this to create a list initially.
+*/
+list make_list();
+
+/*
+PURPOSE: Free all elements.
+USAGE: Each element on the linked list must be dynamically allocated,
+    with no unique pointers to dynamically allocated members.
+*/
+void free_list(list x);
 
 #endif
