c69047dc0aa23161919ab9f58451adf470b47322
Jakob Wyatt <jakob.wyatt@gmail.com>
2019-04-30 15:30:54 +0800

    implemented insert

--- src/list.c
+++ src/list.c
@@ -2,7 +2,37 @@
 
 #include <stddef.h> /*NULL*/
 
-void insert(list x, node* iter, void* data);
+void insert(list x, node* iter, void* data)
+{
+    /*create new node*/
+    node* new = (node*)malloc(sizeof(node));
+    new->data = data;
+
+    /*If iter is null, insert at the end of the list.
+    Else if iter is head, insert at the beginning of the list.
+    Else, reroute nearby nodes.
+    Might be able to clean up logic a bit. Already generalised some LOC
+    to be identical.*/
+    if (iter == NULL)
+    {
+        new->prev = x.tail;
+        new->next = iter;
+        new->prev->next = new;
+        x.tail = new;
+    } else if (iter->prev == NULL)
+    {
+        new->prev = iter->prev;
+        new->next = iter;
+        new->next->prev = new;
+        x.head = new;
+    } else
+    {
+        new->prev = iter->prev;
+        new->next = iter;
+        new->next->prev = new;
+        new->prev->next = new;
+    }
+}
 
 void remove(list x, node* iter)
 {
