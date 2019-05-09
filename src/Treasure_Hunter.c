5c0287e671972c3f1d790fc4b47f79242cad838a
Jakob Wyatt <jakob.wyatt@gmail.com>
2019-05-09 14:00:07 +0800

    added initial top level code for adventure testing

--- src/Treasure_Hunter.c
+++ src/Treasure_Hunter.c
@@ -2,51 +2,46 @@
 #include <stddef.h>
 #include "map.h"
 #include "movement.h"
+#include "adventure.h"
 
 int main(int argc, char* argv[])
 {
-    /*
-    General flow of events:
-    - Read the map into a 2d array. The datatype of this array will be a struct consisting of:
-        An enum, describing what the datatype of the item is.
-        A void ptr, which points to the location of the item.
-    -Read the adventure file into a linked list.
-        Any errors in these first two steps are written to stderr, and changes the status to ABORTED.
-    -Resolve the adventure, keeping track of:
-        Gear equipped.
-        Total magic value.
-        Total coins.
-
-        This is done by iterating through the linked list.
-        Boundary checking is done conditionally.
-        Logging is done via the file adventure.log, and is conditionally written to stdout.
-        If an out of bounds error is encountered, control flow ends and the status is FAILED.
-        If the path is changed, control flow continues and the status is CORRECTED.
-    -Print the output of the program.
-    */
-
     size_t rows;
     size_t cols;
     map x;
     list y;
-    status result;
+    status map_result;
+    status list_result;
+    explorer person;
+    FILE* logger;
     if (argc != 3)
     {
         printf("Usage: ./TreasureHunter <map_file> <movement_file>\n");
     } else
     {
-        result = read_map(&x, &rows, &cols, argv[1]);
-        if (result == COMPLETE)
+        map_result = read_map(&x, &rows, &cols, argv[1]);
+        list_result = read_moves(&y, argv[2]);
+        logger = fopen("adventure.log", "a");
+
+        if (logger != NULL && map_result == COMPLETE && list_result == COMPLETE)
         {
-            print_map(x, rows, cols);
-            result = read_moves(&y, argv[2]);
-            if (result == COMPLETE)
-            {
-                for_each(y, &print_move);
-            }
-            free_map(x, rows, cols);
-            free_list(&y);
+            resolveAdventure(x, rows, cols, y, &person, logger);
         }
+
+        /*clean up*/
+        if (map_result == COMPLETE)
+        {
+            free_map(x, rows, cols);
+        }
+        if (ferror(logger))
+        {
+            fprintf(stderr, "An error occured while writing to the log file.\n");
+        }
+        if (logger != NULL)
+        {
+            fclose(logger);
+        }
+        free_list(&y);
     }
     printf("\n");
     return 0;
