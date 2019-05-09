b5d8ecd9aacfa54368b11e798293f4d7691acd9d
Jakob Wyatt <jakob.wyatt@gmail.com>
2019-05-09 10:37:51 +0800

    added function to make empty explorer

--- src/types.c
+++ src/types.c
@@ -1,1 +1,17 @@
 #include "types.h"
+
+explorer make_explorer()
+{
+    explorer ex;
+    ex.magic = 0;
+    ex.coin = 0;
+    ex.head.type = 'N';
+    ex.head.value = -1;
+    ex.chest.type = 'N';
+    ex.chest.value = -1;
+    ex.legs.type = 'N';
+    ex.legs.value = -1;
+    ex.hands.type = 'N';
+    ex.hands.value = -1;
+    return ex;
+}
