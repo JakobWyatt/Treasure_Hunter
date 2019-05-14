56e33adf0485474113f884222cfd30adcc88eca3
Jakob Wyatt <jakob.wyatt@gmail.com>
2019-05-14 10:00:00 +0800

    modularised test file

--- test/test.py
+++ test/test.py
@@ -1,7 +1,13 @@
 import subprocess
+import os
 
 def test_suite():
-    subprocess.Popen(["make clean && make"], cwd="../src")
+    build()
+    os.remove("../src/adventure.log")
+
+def build():
+    subprocess.call(["make", "clean"], cwd="../src")
+    subprocess.call(["make"], cwd="../src")
 
 if __name__ == "__main__":
     test_suite()
