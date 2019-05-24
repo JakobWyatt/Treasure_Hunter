f4baa9b4f91a420cff9ce09ea51888831a0c9954
Jakob Wyatt <jakob.wyatt@gmail.com>
2019-05-24 18:55:29 +0800

    added implementation comments

--- src/list.c
+++ src/list.c
@@ -2,6 +2,24 @@
 
 #include <stdlib.h> /*NULL*/
 
+/**
+ * \internal \n \n \b Implementation:
+ *  A node named new is dynamically allocated (on the heap),
+ *      and \p data is referenced by it.
+ *  - If \p iter == NULL, insert at the end of the list.
+ *      -# Node before new is \p x->tail.
+ *      -# Node after new is NULL.
+ *      -# \p x->tail is now new.
+ *      -# If there are currently no elements in the list,
+ *              set x->head to new. Otherwise, link the node before
+ *              new to new.
+ *  - Otherwise, the next node is guarenteed to exist.
+ *      -# Node before new is \p iter->prev.
+ *      -# Node after new is iter.
+ *      -# \p iter->prev is new.
+ *      -# If iter->prev is NULL (start of list), \p x->head is new.
+ *          Otherwise, new->prev->next is new.
+ */
 void insert(list* x, node* iter, void* data)
 {
     /*create new node*/
@@ -25,21 +43,32 @@ void insert(list* x, node* iter, void* data)
         {
             new->prev->next = new;
         }
-    } else if (iter->prev == NULL)
-    {
-        new->prev = iter->prev;
-        new->next = iter;
-        new->next->prev = new;
-        x->head = new;
     } else
     {
         new->prev = iter->prev;
         new->next = iter;
         new->next->prev = new;
-        new->prev->next = new;
+        if (iter->prev == NULL)
+        {
+            x->head = new;
+        } else 
+        {
+            new->prev->next = new;
+        }
     }
 }
 
+/**
+ * \internal \n \n \b Implementation:
+ * First, free \p iter->data.
+ * If \p iter is the first element in \p x,
+ *  change the head pointer to \p iter->next.
+ * Otherwise, the previous node.next is equal to the
+ *  next node.
+ * When \p iter is the final element in \p x,
+ *  perform the algorithm above but set the tail pointer
+ *  and the next node.prev instead.
+ */
 void remove_node(list* x, node* iter)
 {
     free(iter->data);
@@ -61,6 +90,10 @@ void remove_node(list* x, node* iter)
     free(iter);
 }
 
+/**
+ * \internal \n \n \b Implementation:
+ * Set head and tail to NULL.
+ */
 list make_list()
 {
     list x;
@@ -69,6 +102,10 @@ list make_list()
     return x;
 }
 
+/**
+ * \internal \n \n \b Implementation:
+ * Call \ref remove_node on \p x->head until x->head is NULL (list is empty).
+ */
 void free_list(list* x)
 {
     while (x->head != NULL)
@@ -77,7 +114,13 @@ void free_list(list* x)
     }
 }
 
-void for_each(list x, for_each_func func)
+/**
+ * \internal \n \n \b Implementation:
+ * -# Apply \p func to the current node, starting at \p x.head.
+ * -# Move the current node forward.
+ * Repeat this until the current node is NULL.
+ */
+void for_each(list x, data_func func)
 {
     node* a = x.head;
     while (a != NULL)
