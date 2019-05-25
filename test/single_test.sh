8b956508b5e60b0fcb93c9d38352832a9198dcb9
Jakob Wyatt <jakob.wyatt@gmail.com>
2019-05-25 18:22:01 +0800

    added initial tests for invalid values

--- test/single_test.sh
+++ test/single_test.sh
@@ -7,15 +7,15 @@ make clean && make
 rm adventure.log
 rm ../test/output.log
 
-valgrind --leak-check=full -v --log-file=../test/valgrind1.log --error-exitcode=1 ./TreasureHunter $1 $2 &>> ../test/output.log
+valgrind --leak-check=full -v --log-file=../test/valgrind1_$5.log --error-exitcode=1 ./TreasureHunter $1 $2 &>> ../test/output.log
 if [ $? == 1 ]; then
     echo "Memory error."
 fi
-valgrind --leak-check=full -v --log-file=../test/valgrind2.log --error-exitcode=1 ./TreasureHunterLog $1 $2 &>> ../test/output.log
+valgrind --leak-check=full -v --log-file=../test/valgrind2_$5.log --error-exitcode=1 ./TreasureHunterLog $1 $2 &>> ../test/output.log
 if [ $? == 1 ]; then
     echo "Memory error."
 fi
-valgrind --leak-check=full -v --log-file=../test/valgrind3.log --error-exitcode=1 ./TreasureHunterAI $1 $2 &>> ../test/output.log
+valgrind --leak-check=full -v --log-file=../test/valgrind3_$5.log --error-exitcode=1 ./TreasureHunterAI $1 $2 &>> ../test/output.log
 if [ $? == 1 ]; then
     echo "Memory error."
 fi
