64041ecc7f90f2f71b7f3ac35208e7edaf0dcc4c
Jakob Wyatt <jakob.wyatt@gmail.com>
2019-05-25 18:41:44 +0800

    assume that all log files will probably be empty

--- test/all_tests.sh
+++ test/all_tests.sh
@@ -14,44 +14,44 @@ bash ../test/single_test.sh ../test/default.csv ../test/empty_list.lst \
     ../test/empty_list.txt ../test/empty.log empty_list
 
 bash ../test/single_test.sh ../test/empty_map.csv ../test/default.lst \
-    ../test/default.txt ../test/default.log empty_map
+    ../test/default.txt ../test/empty.log empty_map
 
 bash ../test/single_test.sh ../test/default.csv ../test/invalid_dir.lst \
-    ../test/default.txt ../test/default.log invalid_dir
+    ../test/default.txt ../test/empty.log invalid_dir
 
 bash ../test/single_test.sh ../test/default.csv ../test/invalid_dist.lst \
-    ../test/default.txt ../test/default.log invalid_dist
+    ../test/default.txt ../test/empty.log invalid_dist
 
 bash ../test/single_test.sh ../test/default.csv ../test/no_space.lst \
-    ../test/default.txt ../test/default.log no_space_list
+    ../test/default.txt ../test/empty.log no_space_list
 
 #test invalid map
 bash ../test/single_test.sh ../test/incorrect_rows.csv ../test/default.lst \
-    ../test/default.txt ../test/default.log incorrect_rows
+    ../test/default.txt ../test/empty.log incorrect_rows
 
 bash ../test/single_test.sh ../test/incorrect_cols.csv ../test/default.lst \
-    ../test/default.txt ../test/default.log incorrect_cols
+    ../test/default.txt ../test/empty.log incorrect_cols
 
 bash ../test/single_test.sh ../test/negative_cols.csv ../test/default.lst \
-    ../test/default.txt ../test/default.log negative_cols
+    ../test/default.txt ../test/empty.log negative_cols
 
 bash ../test/single_test.sh ../test/invalid_rows.csv ../test/default.lst \
-    ../test/default.txt ../test/default.log invalid_rows
+    ../test/default.txt ../test/empty.log invalid_rows
 
 bash ../test/single_test.sh ../test/invalid_treasure_type.csv ../test/default.lst \
-    ../test/default.txt ../test/default.log invalid_treasure_type
+    ../test/default.txt ../test/empty.log invalid_treasure_type
 
 bash ../test/single_test.sh ../test/invalid_slot.csv ../test/default.lst \
-    ../test/default.txt ../test/default.log invalid_slot
+    ../test/default.txt ../test/empty.log invalid_slot
 
 bash ../test/single_test.sh ../test/invalid_value.csv ../test/default.lst \
-    ../test/default.txt ../test/default.log invalid_value
+    ../test/default.txt ../test/empty.log invalid_value
 
 bash ../test/single_test.sh ../test/too_many_colons.csv ../test/default.lst \
-    ../test/default.txt ../test/default.log too_many_colons
+    ../test/default.txt ../test/empty.log too_many_colons
 
 bash ../test/single_test.sh ../test/too_little_colons.csv ../test/default.lst \
-    ../test/default.txt ../test/default.log too_little_colons
+    ../test/default.txt ../test/empty.log too_little_colons
 
 bash ../test/single_test.sh ../test/no_space.csv ../test/default.lst \
-    ../test/default.txt ../test/default.log no_space_map
+    ../test/default.txt ../test/empty.log no_space_map
