e0d0346f0eae915d74a03479bd211f7b3692f3f1
Jakob Wyatt <jakob.wyatt@gmail.com>
2019-05-25 11:38:27 +0800

    added comments to main function

--- src/Treasure_Hunter.c
+++ src/Treasure_Hunter.c
@@ -5,6 +5,11 @@
 #include "adventure.h"
 #include "types.h"
 
+/**
+    \file Treasure_Hunter.c
+    \brief Contains top level main function to run the game.
+*/
+
 int main(int argc, char* argv[])
 {
     size_t rows;
@@ -22,20 +27,23 @@ int main(int argc, char* argv[])
         printf("Usage: ./TreasureHunter <map_file> <movement_file>\n");
     } else
     {
+        /* Initialize the values needed before adventure resolution. */
         map_result = read_map(&x, &rows, &cols, argv[1]);
         list_result = read_moves(&y, argv[2]);
         logger = fopen("adventure.log", "a");
         person = make_explorer();
 
+        /* Ensure that file reading worked properly. */
         if (logger != NULL && map_result == COMPLETE && list_result == COMPLETE)
         {
+            /* Resolve the adventure and print the result. */
             adventure_result = resolveAdventure(x, rows, cols, y, &person, logger);
             status_text(adventure_result, final_status);
             printf("STATUS: %s\nCOINS: %d\nMAGIC: %d\nGEAR: %d\n", final_status,
                 person.coin, person.magic, gear_value(person));
         }
 
-        /*clean up*/
+        /* Clean up memory and files. */
         if (map_result == COMPLETE)
         {
             free_map(x, rows, cols);
