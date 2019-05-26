7ae5861c87c8b759bc85089e8ce04014dfdd90db
Jakob Wyatt <jakob.wyatt@gmail.com>
2019-05-26 16:16:58 +0800

    added some more expected output

--- test/all_tests.sh
+++ test/all_tests.sh
@@ -13,9 +13,6 @@ bash ../test/single_test.sh ../test/bounds.csv ../test/bounds.lst \
 bash ../test/single_test.sh ../test/default.csv ../test/empty_list.lst \
     ../test/empty_list.txt ../test/empty.log empty_list
 
-bash ../test/single_test.sh ../test/empty_map.csv ../test/default.lst \
-    ../test/empty_map.txt ../test/empty.log empty_map
-
 bash ../test/single_test.sh ../test/default.csv ../test/invalid_dir.lst \
     ../test/invalid_dir.txt ../test/empty.log invalid_dir
 
@@ -28,6 +25,9 @@ bash ../test/single_test.sh ../test/default.csv ../test/no_space.lst \
     ../test/no_space_list.txt ../test/empty.log no_space_list
 
 #test invalid map
+bash ../test/single_test.sh ../test/empty_map.csv ../test/default.lst \
+    ../test/empty_map.txt ../test/empty.log empty_map
+
 bash ../test/single_test.sh ../test/incorrect_rows.csv ../test/default.lst \
     ../test/incorrect_rows.txt ../test/empty.log incorrect_rows
 
@@ -40,14 +40,16 @@ bash ../test/single_test.sh ../test/negative_cols.csv ../test/default.lst \
 bash ../test/single_test.sh ../test/invalid_rows.csv ../test/default.lst \
     ../test/invalid_rows.txt ../test/empty.log invalid_rows
 
+#Row error message is accurate but may be misinterpreted
 bash ../test/single_test.sh ../test/invalid_treasure_type.csv ../test/default.lst \
-    ../test/default.txt ../test/empty.log invalid_treasure_type
+    ../test/invalid_treasure_type.txt ../test/empty.log invalid_treasure_type
 
+#Generic error message - still gives the correct position though
 bash ../test/single_test.sh ../test/invalid_slot.csv ../test/default.lst \
-    ../test/default.txt ../test/empty.log invalid_slot
+    ../test/invalid_slot.txt ../test/empty.log invalid_slot
 
 bash ../test/single_test.sh ../test/invalid_value.csv ../test/default.lst \
-    ../test/default.txt ../test/empty.log invalid_value
+    ../test/invalid_value.txt ../test/empty.log invalid_value
 
 bash ../test/single_test.sh ../test/too_many_colons.csv ../test/default.lst \
     ../test/default.txt ../test/empty.log too_many_colons
